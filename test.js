51c2f4555e25ccbf09d6d7670e04dc75adb184cd
Zhuug <154862334+Zhuug@users.noreply.github.com>
2024-02-16 14:08:25 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,24 @@
+let AppData = {};
 
+function loadDataFileFR(file, onload) {
+	let fr = new FileReader();
+	fr.onload = function () {
+		try {
+			let obj = JSON.parse(fr.result, reviver);
+			console.log(obj);
+      AppData.obj = obj;
+			onload();
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	}
+	fr.readAsText(file);
+}
+
+function AppData.ShowData() {
+}
+
+window.onload = function() {
+  loadDocFileFR("./data/list-20230104.tsv", AppData.ShowData)
+}
