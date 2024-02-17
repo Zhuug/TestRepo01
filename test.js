7cc8075976e13df66b390bb574ce10adff1c1c72
Zhuug <154862334+Zhuug@users.noreply.github.com>
2024-02-17 14:52:04 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,33 +1,34 @@
 let AppData = {
 
-obj: {},
-
-LoadDataFileFR(file, onload) {
-	let fr = new FileReader();
-	fr.onload = function () {
-		try {
-			let obj = JSON.parse(fr.result, reviver);
-			console.log(obj);
-			this.obj = obj;
-			onload();
-		} catch (error) {
-			console.error(error);
-			return null;
+	obj: {},
+	
+	LoadDataFileFR(file, onload) {
+		let fr = new FileReader();
+		fr.onload = function () {
+			try {
+				let obj = JSON.parse(fr.result, reviver);
+				console.log(obj);
+				this.obj = obj;
+				onload();
+			} catch (error) {
+				console.error(error);
+				return null;
+			}
 		}
-	}
-	fr.readAsText(file);
-},
-
-ShowData() {
-},
-
+		fr.readAsText(file);
+	},
+	
+	ShowData() {
+	},
+	
 }
-
+	
 window.onload = async function() {
-const response = await fetch("./data/list-20230104.tsv");
-if (response && response.ok) {
-	const json = await response.json();
-	console.log(json);
-} else {
-	console.warn('bleh',response);
+	const response = await fetch("./data/list-20230104.tsv");
+	if (response && response.ok) {
+		const json = await response.json();
+		console.log(json);
+	} else {
+		console.warn('bleh',response);
+	}
 }
